e7c7a27629af048113a357b2338c062c9a99edd5
jc <root@sha3.dev>
2023-02-28 20:10:10 +0100

    first commit

---
+++ src/lib/imgproxy.ts
@@ -0,0 +1,55 @@
+/**
+ * imports: externals
+ */
+
+import createHmac from "create-hmac";
+import path from "path";
+
+/**
+ * export
+ */
+
+export class Imgproxy {
+  /**
+   * constructor
+   */
+  constructor(private options: { baseUrl: string; key: string; salt: string; presets: string[]; extension?: string }) {}
+  /**
+   * private static : methods
+   */
+
+  private static urlSafeBase64(url: string) {
+    Buffer.from(url).toString("base64").replace(/=/g, "").replace(/\+/g, "-").replace(/\//g, "_");
+  }
+
+  private static hexDecode(hex: string) {
+    Buffer.from(hex, "hex");
+  }
+
+  /**
+   * private : methods
+   */
+
+  private sign(target: string) {
+    const hmac = createHmac("sha256", Imgproxy.hexDecode(this.options.key));
+    hmac.update(Imgproxy.hexDecode(this.options.salt));
+    hmac.update(target);
+    return Imgproxy.urlSafeBase64(hmac.digest());
+  }
+
+  /**
+   * public : methods
+   */
+
+  getPresetsUrls(url: string) {
+    const result: Record<string, string> = {};
+    const extname = this.options.extension || path.extname(url);
+    this.options.presets.forEach((preset) => {
+      const safeUrl = Imgproxy.urlSafeBase64(url);
+      const safePath = `/${preset}/${safeUrl}${extname}`;
+      const signature = this.sign(safePath);
+      result[preset] = `${this.options.baseUrl}/${signature}${safePath}`;
+    });
+    return result;
+  }
+}
